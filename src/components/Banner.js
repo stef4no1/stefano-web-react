e7f00f5bfe61eebf19cc619496c2e3d905eec02c
Stefano Straga <stefanostraga2k18@gmail.com>
2022-11-12 20:15:14 +0100

    actualizando banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,7 @@
+import { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import headerImg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
     return (
@@ -7,6 +10,14 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
+                        <h1>{`Hi Im Stefano Straga`}<span className="wrap">Web developer</span></h1>
+                        <p>
+                            I am a full stack programming student, I like the backend and everything that has to do with web3, passionate about cryptocurrencies and programming, I would like to be able to work on it one day, my favorite programming languages are C#, PHP and JS
+                        </p>
+                        <button onClick={() => console.log('connect')}>Lets Connect <ArrowRightCircle size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img"/>
                     </Col>
                 </Row>
             </Container>
