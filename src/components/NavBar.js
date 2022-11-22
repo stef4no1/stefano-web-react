f997d3c476031172a989b3c3246be62cb99d14bf
Stefano Straga <stefanostraga2k18@gmail.com>
2022-11-22 19:01:51 +0100

    upload navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,9 +50,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+              <a href="https://www.linkedin.com/in/stragastefano/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="Icon" /></a>
+                <a href="https://www.facebook.com/stefano.straga" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="Icon" /></a>
+                <a href="https://www.instagram.com/__stef4no/" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt="Icon" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Contact me</span></button>
